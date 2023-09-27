94aadd729806659fa2d64ba190f6c13cab462523
Kevin Rivera <shimpo11.kr@gmail.com>
2023-09-27 14:03:20 -0500

    Add TypeOrmModule de la entidad BienesCategoria

---
+++ src/bienes-categorias/bienes-categorias.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BienesCategoriasController } from './bienes-categorias.controller';
+import { BienesCategoriasService } from './bienes-categorias.service';
+import { BienesCategoria } from './entities/bienes-categoria.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([BienesCategoria])],
+  controllers: [BienesCategoriasController],
+  providers: [BienesCategoriasService],
+})
+export class BienesCategoriasModule {}
