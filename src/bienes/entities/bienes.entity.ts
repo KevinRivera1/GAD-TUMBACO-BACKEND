a5829bd7ed1d276ea88b370f4301bb8f0964d812
Kevin Rivera <shimpo11.kr@gmail.com>
2023-09-27 14:02:02 -0500

    Add entidad Bienes campos

---
+++ src/bienes/entities/bienes.entity.ts
@@ -0,0 +1,102 @@
+import { BienesCategoria } from "src/bienes-categorias/entities/bienes-categoria.entity";
+import { BienesSolicitud } from "src/bienes-solicitud/entities/bienes-solicitud.entity";
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity({ name: "bienes" })
+export class Bienes {
+  @PrimaryGeneratedColumn()
+  id_bienes: number;
+
+  @Column({
+    name: "responsable_bien",
+    type: "varchar",
+    length: 100,
+    comment: "Se registrará el propietario o titular del bien prestado",
+  })
+  responsable_bien: string;
+
+  @Column({
+    name: "nombre_bien",
+    type: "varchar",
+    length: 100,
+    comment: "Se registrará el nombre o título del bien",
+  })
+  nombre_bien: string;
+
+  @Column({
+    name: "marca",
+    type: "varchar",
+    length: 100,
+    comment: "Se registrará la marca correspondiente del bien",
+  })
+  marca: string;
+
+  @Column({
+    name: "serie",
+    type: "varchar",
+    length: 50,
+    comment:
+      "Se registrará un numero serial correspondiente al bien para identificarlos a cada uno",
+  })
+  serie: string;
+
+  @Column({
+    name: "detalle",
+    type: "varchar",
+    length: 100,
+    comment: "Se registrará cualquier situación o novedad referente al bien",
+  })
+  detalle: string;
+
+  @Column({
+    name: "stock",
+    type: "int",
+    comment:
+      "Se registrará la cantidad disponible del bien para realizar cualquier prestación",
+  })
+  stock: number;
+
+  @Column({
+    name: "disponibilidad",
+    type: "bool",
+    comment:
+      "Se registrará la cantidad disponible del bien para realizar cualquier prestación",
+  })
+  disponibilidad: boolean;
+
+  // Relación muchos a uno con la tabla categorias
+  @ManyToOne(() => BienesCategoria, (categorias) => categorias.bienes)
+  @JoinColumn({ name: "id_categorias" })
+  categorias: BienesCategoria;
+
+
+  @OneToMany(() => BienesSolicitud, solicitud => solicitud.bienes)
+  @JoinColumn({ name: "id_solicitud_bienes" })
+  bienesSolicitud: BienesSolicitud;
+
+  @CreateDateColumn({
+    name: "created_at",
+    type: "timestamp",
+    default: () => "CURRENT_TIMESTAMP",
+  })
+  createAt: Date;
+
+  @UpdateDateColumn({
+    name: "updated_at",
+    type: "timestamp",
+  })
+  updateAt: Date;
+
+  @DeleteDateColumn({ name: "deleted_at", type: "timestamp" })
+  deleteAt: Date;
+}
