f961ab17f0bcdf05709b4ce9dde5daacbc3e2659
Kevin Rivera <shimpo11.kr@gmail.com>
2023-09-27 14:00:42 -0500

    Add Servicios de la entidad Bienes y repository

---
+++ src/bienes/bienes.service.ts
@@ -0,0 +1,54 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateBienDto } from './dto/create-bien.dto';
+import { UpdateBienDto } from './dto/update-bien.dto';
+import { Bienes } from './entities/bienes.entity';
+
+@Injectable()
+export class BienesService {
+
+  constructor(
+    @InjectRepository(Bienes)
+    private readonly bienesRepository: Repository<Bienes>,
+  ) { }
+
+
+  async findAll() {
+    //return await this.bienesRepository.find({where : {disponibilidad:false}});
+    return await this.bienesRepository.find();
+  }
+
+
+  async create(createBienDto: CreateBienDto) {
+    const bienes = this.bienesRepository.create(createBienDto);
+    return await this.bienesRepository.save(bienes);
+  }
+
+
+  async findOne(id: number): Promise<Bienes> {
+    const bienes = await this.bienesRepository.findOneBy({ id_bienes: id });
+    if (!bienes) throw new NotFoundException("No se pudo encontrar el Registro de Bienes con el ID proporcionado");
+    return bienes;
+  }
+
+  async update(id: number, updateBienDto: UpdateBienDto) {
+    return await this.bienesRepository.update(id, updateBienDto);
+  }
+
+  //* eliminacion fisica
+  remove(id: number) {
+    return `This action removes a #${id} biene`;
+  }
+
+  //* eliminacion logica
+  async deleteSoftBien(id : number): Promise<void> {
+    await this.bienesRepository.softDelete(id);
+  }
+
+  //* restauracion logica
+  async restoreBien(id : number): Promise<void> {
+    await this.bienesRepository.restore(id);
+  }
+
+}
