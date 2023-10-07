5fb2250e48d1fd58f224e019c37792a80a076fbf
Kevin Rivera <shimpo11.kr@gmail.com>
2023-10-07 16:15:59 -0500

    [Add mejora] Se mejora la implementacionde del metodo create y update de bienesCategoria

--- src/bienes-categorias/bienes-categorias.service.ts
+++ src/bienes-categorias/bienes-categorias.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateBienesCategoriaDto } from './dto/create-bienes-categoria.dto';
@@ -7,7 +7,9 @@ import { BienesCategoria } from './entities/bienes-categoria.entity';
 
 @Injectable()
 export class BienesCategoriasService {
-  
+
+  private categorias: BienesCategoria[] = [];
+
   constructor(
     @InjectRepository(BienesCategoria)
     private readonly categoriasRepository: Repository<BienesCategoria>,
@@ -17,11 +19,30 @@ export class BienesCategoriasService {
     //return await this.bienesRepository.find({where : {disponibilidad:false}});
     return await this.categoriasRepository.find();
   }
-  
+
   async create(createBienesCategoriaDto: CreateBienesCategoriaDto) {
-    const categorias = this.categoriasRepository.create(createBienesCategoriaDto);
-    return await this.categoriasRepository.save(categorias);
+
+    const existingCategoria = await this.categoriasRepository.findOneBy({
+      nombre_categoria: createBienesCategoriaDto.nombre_categoria,
+    });
+
+
+    const existingInMemory = this.categorias.find(
+      (categoria) => categoria.nombre_categoria === createBienesCategoriaDto.nombre_categoria
+    );
+
+
+    if (existingCategoria || existingInMemory) {
+      throw new ConflictException(`La categoría ${createBienesCategoriaDto.nombre_categoria} ya existe`);
+    }
+
+    const nuevaCategoria = this.categoriasRepository.create(
+      createBienesCategoriaDto
+    );
+    this.categorias.push(nuevaCategoria);
+    return await this.categoriasRepository.save(nuevaCategoria);
   }
+
 
   async findOne(id: number): Promise<BienesCategoria> {
     const categorias = await this.categoriasRepository.findOneBy({ id_categorias: id });
@@ -30,7 +51,26 @@ export class BienesCategoriasService {
   }
 
   async update(id: number, updateBienesCategoriaDto: UpdateBienesCategoriaDto) {
-    return await this.categoriasRepository.update(id, updateBienesCategoriaDto);
+    //return await this.categoriasRepository.update(id, updateBienesCategoriaDto);
+
+    const categoria = await this.categoriasRepository.findOneBy({ id_categorias: id });
+    if (!categoria) throw new NotFoundException("No se pudo encontrar la categoria con el ID proporcionado");
+
+    const existingCategoria = await this.categoriasRepository.findOneBy({
+      nombre_categoria: updateBienesCategoriaDto.nombre_categoria,
+    });
+
+    const existingInMemory = this.categorias.find(
+      (categoria) => categoria.nombre_categoria === updateBienesCategoriaDto.nombre_categoria
+    );
+
+    if (existingCategoria || existingInMemory) {
+      throw new ConflictException(`La categoría ${updateBienesCategoriaDto.nombre_categoria} ya existe`);
+    }
+
+    const editedCategoria = Object.assign(categoria, updateBienesCategoriaDto);
+    return await this.categoriasRepository.save(editedCategoria);
+
   }
 
   //* eliminacion fisica
@@ -39,12 +79,12 @@ export class BienesCategoriasService {
   }
 
   //* eliminacion logica
-  async deleteSoftCategoria(id : number): Promise<void> {
+  async deleteSoftCategoria(id: number): Promise<void> {
     await this.categoriasRepository.softDelete(id);
   }
 
   //* restauracion logica
-  async restoreCategoria(id : number): Promise<void> {
+  async restoreCategoria(id: number): Promise<void> {
     await this.categoriasRepository.restore(id);
   }
 }
