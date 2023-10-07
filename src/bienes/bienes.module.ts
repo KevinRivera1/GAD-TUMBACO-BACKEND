d43cdb038f4513a836d9345232b8e453ed163442
Kevin Rivera <shimpo11.kr@gmail.com>
2023-10-07 14:50:43 -0500

    feat: :art: Agregar módulo BienesModule
    Agregar un nuevo módulo BienesModule que incluye las importaciones de TypeOrmModule para Bienes y BienesCategoriasModule, así como los controladores y proveedores asociados.

--- src/bienes/bienes.module.ts
+++ src/bienes/bienes.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { BienesCategoriasModule } from 'src/bienes-categorias/bienes-categorias.module';
+import { BienesCategoriasService } from 'src/bienes-categorias/bienes-categorias.service';
 import { BienesController } from './bienes.controller';
 import { BienesService } from './bienes.service';
 import { Bienes } from './entities/bienes.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Bienes])],
+  imports: [TypeOrmModule.forFeature([Bienes]),BienesCategoriasModule],
   controllers: [BienesController],
-  providers: [BienesService],
+  providers: [BienesService,BienesCategoriasService],
 })
 export class BienesModule {}
