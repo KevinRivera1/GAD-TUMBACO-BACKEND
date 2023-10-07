3b3c227ea2dfc3162fd095291e443a71cebe6965
Kevin Rivera <shimpo11.kr@gmail.com>
2023-10-07 15:22:01 -0500

    [Export] se exporto el typeormModule de bienesModule

--- src/bienes/bienes.module.ts
+++ src/bienes/bienes.module.ts
@@ -1,14 +1,15 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { BienesCategoriasModule } from 'src/bienes-categorias/bienes-categorias.module';
-import { BienesCategoriasService } from 'src/bienes-categorias/bienes-categorias.service';
-import { BienesController } from './bienes.controller';
-import { BienesService } from './bienes.service';
-import { Bienes } from './entities/bienes.entity';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { BienesCategoriasModule } from "src/bienes-categorias/bienes-categorias.module";
+import { BienesCategoriasService } from "src/bienes-categorias/bienes-categorias.service";
+import { BienesController } from "./bienes.controller";
+import { BienesService } from "./bienes.service";
+import { Bienes } from "./entities/bienes.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Bienes]),BienesCategoriasModule],
+  imports: [TypeOrmModule.forFeature([Bienes]), BienesCategoriasModule],
   controllers: [BienesController],
-  providers: [BienesService,BienesCategoriasService],
+  providers: [BienesService, BienesCategoriasService],
+  exports: [TypeOrmModule],
 })
 export class BienesModule {}
