957e93237d4d04a13a3481dcf1d8e165cdd24805
Kevin Rivera <shimpo11.kr@gmail.com>
2023-10-07 14:20:38 -0500

    Se exporta el typeOrmModule para qpoder utilizar en el servicio de bienes

--- src/bienes-categorias/bienes-categorias.module.ts
+++ src/bienes-categorias/bienes-categorias.module.ts
@@ -1,12 +1,13 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { BienesCategoriasController } from './bienes-categorias.controller';
-import { BienesCategoriasService } from './bienes-categorias.service';
-import { BienesCategoria } from './entities/bienes-categoria.entity';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { BienesCategoriasController } from "./bienes-categorias.controller";
+import { BienesCategoriasService } from "./bienes-categorias.service";
+import { BienesCategoria } from "./entities/bienes-categoria.entity";
 
 @Module({
   imports: [TypeOrmModule.forFeature([BienesCategoria])],
   controllers: [BienesCategoriasController],
   providers: [BienesCategoriasService],
+  exports: [TypeOrmModule],
 })
 export class BienesCategoriasModule {}
