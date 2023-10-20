4b0775867d48563b882b391b8ea2259cb49fe6a7
Michael7543 <davidmichael7543@hotmail.com>
2023-10-19 20:50:34 -0500

    se agrego el insert into de la tabla usuario

--- src/Database/database.service.ts
+++ src/Database/database.service.ts
@@ -14,6 +14,21 @@ export class DatabaseService {
       database: process.env.NAME_DB,
     });
   }
+
+    //Se verifica si ya estan creado el usuario presidencia antes de insertar
+
+    async usuariopresidenciaExists(): Promise<boolean> {
+      const client = await this.pool.connect();
+      try {
+        const query = `
+        SELECT COUNT(*) FROM usuarios WHERE id_usuarios = 1;
+      `;
+        const result = await client.query(query);
+        return result.rows[0].count > 0;
+      } finally {
+        client.release();
+      }
+    }
   //Se verifica si ya estan creados primeramente antes de insertar
 
   async estadoExistsActivo(): Promise<boolean> {
@@ -84,7 +99,7 @@ export class DatabaseService {
     const client = await this.pool.connect();
     try {
       const query = `
-        SELECT COUNT(*) FROM roles WHERE nombre = 'Contabilidad';
+        SELECT COUNT(*) FROM roles WHERE nombre = 'Bienes';
       `;
       const result = await client.query(query);
       return result.rows[0].count > 0;
@@ -101,58 +116,64 @@ export class DatabaseService {
       // Verifica si ya existe estado 'Activo'
       if (!(await this.estadoExistsActivo())) {
         const queryestadoActivo = `
-      INSERT INTO estados (id_estados,nombre, acronimo,descripcion)
-      VALUES (1,'Activo','EA', 'Estado activo del Usuario');
-    `;
+          INSERT INTO estados (id_estados, nombre, acronimo, descripcion)
+          VALUES (1, 'Activo', 'EA', 'Estado activo del Usuario');
+        `;
         await client.query(queryestadoActivo);
       }
       // Verifica si ya existe estado 'Inactivo'
       if (!(await this.estadoExistsInactivo())) {
         const queryestadoInactivo = `
-      INSERT INTO estados (id_estados,nombre, acronimo,descripcion)
-      VALUES (2,'Inactivo','IA', 'Estado inactivo del Usuario');
-    `;
+          INSERT INTO estados (id_estados, nombre, acronimo, descripcion)
+          VALUES (2, 'Inactivo', 'IA', 'Estado inactivo del Usuario');
+        `;
         await client.query(queryestadoInactivo);
       }
       // Verifica si ya existe 'Presidencia'
       if (!(await this.roleExistsPresidencia())) {
         const queryPresidencia = `
-          INSERT INTO roles (id_roles,nombre, descripcion)
-          VALUES (1,'Presidencia', 'Se administra el modulo de Presidencia');
+          INSERT INTO roles (id_roles, nombre, descripcion)
+          VALUES (1, 'Presidencia', 'Se administra el modulo de Presidencia');
         `;
         await client.query(queryPresidencia);
       }
-
       // Verifica si ya existe 'Proyectos'
       if (!(await this.roleExistsProyectos())) {
         const queryProyectos = `
-          INSERT INTO roles (id_roles,nombre, descripcion)
-          VALUES (2,'Proyectos', 'Se administra el modulo de Proyectos');
+          INSERT INTO roles (id_roles, nombre, descripcion)
+          VALUES (2, 'Proyectos', 'Se administra el modulo de Proyectos');
         `;
         await client.query(queryProyectos);
       }
-
       // Verifica si ya existe 'Secretaria'
       if (!(await this.roleExistsSecretaria())) {
         const querySecretaria = `
-          INSERT INTO roles (id_roles,nombre, descripcion)
-          VALUES (3,'Secretaria', 'Se administra el modulo de Secretaria');
+          INSERT INTO roles (id_roles, nombre, descripcion)
+          VALUES (3, 'Secretaria', 'Se administra el modulo de Secretaria');
         `;
         await client.query(querySecretaria);
       }
-
-      // Verifica si ya existe 'Contabilidad'
+      // Verifica si ya existe 'Bienes'
       if (!(await this.roleExistsContabilidad())) {
         const queryContabilidad = `
-          INSERT INTO roles (id_roles,nombre, descripcion)
-          VALUES (4,'Contabilidad', 'Se administra el modulo de Contabilidad');
+          INSERT INTO roles (id_roles, nombre, descripcion)
+          VALUES (4, 'Bienes', 'Se administra el modulo de Bienes');
         `;
         await client.query(queryContabilidad);
       }
-
-      // Agrega más inserciones aquí, verificando si ya existen.
+      
+      // Realiza la inserción de usuarios al final
+      // Verifica si ya existe usuario 'Presidencia'
+      if (!(await this.usuariopresidenciaExists())) {
+        const queryusuariopresidencia = `
+          INSERT INTO usuarios (id_usuarios, nombres, apellidos, clave, correo, identificacion, celular, id_roles, id_estado)
+          VALUES (1, 'Gad Tumbaco', 'Tumbaco', 'tumbaco2023', 'tumbaco@hotmail.com', '', '', 1, 1);
+        `;
+        await client.query(queryusuariopresidencia);
+      }
     } finally {
       client.release();
     }
   }
+  
 }
